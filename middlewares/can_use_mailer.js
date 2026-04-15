8043e90db88434c3f2cb6e1720e9cd31f69f247a
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-15 07:52:41 +0530

    logging

--- middlewares/can_use_mailer.js
+++ middlewares/can_use_mailer.js
@@ -3,14 +3,14 @@ import { canApplicationUseService } from "../db/application_services.js";
 const { CONSTANTS, logger } = require("@hammerbyte/utils");
 
 export default async function canUseMailer({ application, set }) {
-
     const MAILER_SERVICE_ID = 1;
+
+    console.log(application);
 
     const applicationService = await canApplicationUseService({ application_id: application?.id, service_id: MAILER_SERVICE_ID });
 
     //check if such application does exist and is active
     if (!application || !applicationService) {
-       
         logger.error(`${CONSTANTS.SAAS.SERVICES.MAILER} Service Request Rejected`);
         set.status = 401;
         return { error: ERRORS.UNAUTHORIZED };
