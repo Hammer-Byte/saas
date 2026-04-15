b01622dacaedef3fd7c1c92eec8dd1c047f35c84
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-15 07:48:19 +0530

    logging

--- db/application_services.js
+++ db/application_services.js
@@ -2,10 +2,9 @@ import { executeSQLQuery } from "../libs/db";
 
 const { logger } = require("@hammerbyte/utils");
 
-
-
-
 export async function canApplicationUseService({ application_id, service_id }) {
+    logger.info(`Verifying Application Service - Application : ${application_id} , Service : ${service_id}`);
+
     return await executeSQLQuery(
         (sql) => sql` SELECT * FROM APPLICATION_SERVICES WHERE application_id=${application_id} AND service_id=${service_id} AND active=TRUE`,
     )
