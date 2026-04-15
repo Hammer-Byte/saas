dfc64e322c57c3d9fc5e851f1181fd001ba1fd2a
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-15 07:56:10 +0530

    logging more

--- db/applications.js
+++ db/applications.js
@@ -2,11 +2,9 @@ import { executeSQLQuery } from "../libs/db";
 
 const { logger } = require("@hammerbyte/utils");
 
-
-export async function getActiveApplicationByIdAndToken({ id,token}) {
-    return await executeSQLQuery(
-        (sql) => sql` SELECT * FROM APPLICATIONS WHERE id=${id} AND token=${token} AND active=TRUE`,
-    )
-    .then((result) => (result.length ? result[0] : false))
+export async function getActiveApplicationByIdAndToken({ id, token }) {
+    logger.info(`Getting Application : ${id}  By Token : ${token}`);
+    return await executeSQLQuery((sql) => sql`SELECT * FROM APPLICATIONS WHERE id=${id} AND token=${token} AND active=TRUE`)
+        .then((result) => (result.length ? result[0] : false))
         .catch((error) => logger.error(`getActiveApplicationByIdAndToken: ${error}`));
 }
