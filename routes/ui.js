c9441505bdaf5d4e6072a91ec0ba08c283efb6e7
nisarg <hammerbyte.nisarg@gmail.com>
2026-06-26 11:24:07 +0530

    fake login

--- routes/ui.js
+++ routes/ui.js
@@ -7,6 +7,17 @@ export const uiRoutes = new Elysia()
             message: "Welcome to the separated UI Route!",
         })
     )
+    .get("/login", ({ render }) =>
+        render("login", {
+            title: "Login",
+        })
+    )
+    .get("/verification", ({ render, query }) =>
+        render("verification", {
+            title: "Verify OTP",
+            email: query.email ?? "",
+        })
+    )
     .get("/about", ({ render }) =>
         render("index", {
             title: "About Us",
